761786790224c9f5c0cd94b982a0278d10820d0a
Walking-Tsunami <s.sudan@share.epsb.ca>
2024-11-28 14:40:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+document.getElementById("id_one").addEventListener("click", Btn1);
+
+function Btn1() {
+  let output = "Never";
+
+  alert(output);
+}
+
+document.getElementById("id_two").addEventListener("click", Btn2);
+
+function Btn2() {
+  let output2 = "Cake";
+
+  alert(output2);
+}
+document.getElementById("id_three").addEventListener("click", Btn3);
+
+function Btn3() {
+  let output3 = "Stone";
+
+  alert(output3);
+}
+document.getElementById("id_four").addEventListener("click", Btn4);
+
+function Btn4() {
+  let output4 = "Gain";
+
+  alert(output4);
+}
+document.getElementById("id_five").addEventListener("click", Btn5);
+
+function Btn5() {
+  let output5 = "Ice";
+
+  alert(output5);
+}
